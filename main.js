1556ebd8e0b94d094e8dcbe1747a91828001bf21
Mohammed roshdy <xmedox20111@gmail.com>
2023-07-17 18:20:29 +0300

    init commit

---
+++ main.js
@@ -0,0 +1,29 @@
+import { tasks, renderTasks, saveTasksToLocalStorage } from './Add-Remove.js';
+
+const taskInput = document.getElementById('task-input');
+
+// assigning the object to the array with the input value
+function addTask(description) {
+  const newTask = {
+    description,
+    completed: false,
+    index: tasks.length + 1,
+  };
+  tasks.push(newTask);
+  renderTasks();
+  saveTasksToLocalStorage();
+  taskInput.value = '';
+}
+
+taskInput.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter' && taskInput.value !== '') {
+    // important part which takes the value data coming from the input to our object in the array
+    addTask(taskInput.value);
+  }
+});
+
+document.querySelector('.sign').addEventListener('click', () => {
+  if (taskInput.value !== '') {
+    addTask(taskInput.value);
+  }
+});
